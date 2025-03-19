89f416be5f6caef467888142a507ddea73798cd3
Rowney Ouma <oumarowney@gmail.com>
2025-03-19 23:52:59 +0300

    interpolating variable

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = "ROWNEY OUMA"
+
+const welcomeMessage =  `Welcome to Flatbook, , ${currentUser}!`;
+
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, , ${currentUser}!`;
+
+const oldeUser = "RO"
+
+let shortGreeting = `Welcome, " ${oldeUser}!`;
+
